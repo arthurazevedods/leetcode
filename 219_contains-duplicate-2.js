45547f3d0a8a2bea641b8e8f41f7cbdac45b75ac
Arthur Silva <arthurazevedods@gmail.com>
2024-01-29 18:45:21 -0300

    219

---
+++ 219_contains-duplicate-2.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      if (map.has(nums[i])) {
+        const j = map.get(nums[i]);
+        if (Math.abs(i - j) <= k) {
+          return true;
+        }
+      }
+      map.set(nums[i], i);//adiciona a primeira ocorrencia no Map
+    }
+    return false;
+  };
