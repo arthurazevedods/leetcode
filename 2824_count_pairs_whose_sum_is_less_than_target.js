9d9da7ec0d61fed983b143576e8c3992e63bc9a0
arthur <arthurazevedods@gmail.com>
2024-07-05 11:45:28 -0300

    2824

---
+++ 2824_count_pairs_whose_sum_is_less_than_target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    for (let i = 0;i < nums.length-1;i++){
+        for (let j = 1; j < nums.length; j++){
+            if(i!=j && i < j){
+                if((nums[i] + nums[j]) < target) count++
+            } 
+        }
+    }
+    return count
+};
