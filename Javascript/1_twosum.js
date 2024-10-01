c621867a98eda01e37d307208d4c6f9db03f6e76
arthur <arthurazevedods@gmail.com>
2024-10-01 11:36:38 -0300

    2

--- Javascript/1_twosum.js
+++ Javascript/1_twosum.js
@@ -1,28 +1,3 @@
-//accept
-
-/*
-143ms
-*/
-var twoSum = function(nums, target) {
-    let first;
-    let second;
-    
-    for (let i = 0; i < nums.length; i++){
-        first = nums[i];
-        for (let j = 0; j < nums.length; j++){
-            second = nums[j];
-            if (i != j){
-                if ((first + second) == target ){
-                    let listNums = [i,j];
-                    return listNums ;
-                }
-            }
-        }
-    }
-};
-
-/*
-56 ms
 var twoSum = function(nums, target) {
     const map = new Map()
     for(let i = 0; i < nums.length; i++){
@@ -32,4 +7,3 @@ var twoSum = function(nums, target) {
         }map.set(nums[i],i)
     }
 };
-*/
